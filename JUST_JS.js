23c28c5fb0d7240622a5d309a87752369351afd3
_seshie_🦦 <164806836+Damseshie666@users.noreply.github.com>
2024-06-07 19:58:52 +0000

    Add files via upload

---
+++ JUST_JS.js
@@ -0,0 +1,37 @@
+//alerts in adding behaviour to websites using chrome
+alert("HELLO"); //HELLO
+
+typeof("Dorcas"); //String
+prompt("What is your name?"); //Space asking one to input his or her name
+
+//Creating variables
+var myName = "Seshie"; 
+var yourName = prompt("What is your name?");
+alert("My name is " + myName + ",welcome to my github page" + yourName + "!");
+
+var gameLevel = 1;
+gameLevel = 3;
+alert("Your level is currently :" + gameLevel);
+
+//Javascript variable exercise
+function test(){
+    var a = 3;
+    var b = 8;
+
+    //bucket switch
+    var c = a;
+    a = b;
+    b = c;
+
+
+    console.log("a is" + a);
+    console.log("b is" + b);
+}
+
+//Length
+var name ="Dorcas";
+name.length;
+name.slice(0,1);
+
+
+
